d13a47fc9a3462978dd9816dded11a3d3c1933a4
weiweitoo <sengweitoo@gmail.com>
2018-09-12 00:41:56 +0800

    Add database

--- index.js
+++ index.js
@@ -3,10 +3,36 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// Database
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => {
+	  let result = ''
+	  const times = process.env.TIMES || 5
+	  for (i = 0; i < times; i++) {
+	    result += i + ' '
+	  }
+	  res.send(result)
+	})
+  .get('/db', async (req, res) => {
+	  try {
+	    const client = await pool.connect()
+	    const result = await client.query('SELECT * FROM test_table');
+	    res.render('pages/db', result);
+	    client.release();
+	  } catch (err) {
+	    console.error(err);
+	    res.send("Error " + err);
+	  }
+	})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
