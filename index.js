6fbab3118fb6abedc51cc9e8feb2530386b879f3
weiweitoo <sengweitoo@gmail.com>
2018-09-24 00:21:56 +0800

    Add new route time

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/times', (req, res) => {
+	  let result = ''
+	  const times = process.env.TIMES || 5
+	  for (i = 0; i < times; i++) {
+	    result += i + ' '
+	  }
+	  res.send(result)
+	});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
