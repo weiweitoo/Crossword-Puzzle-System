35516c27c811474427440696b969908221578664
weiweitoo <sengweitoo@gmail.com>
2018-09-24 03:31:07 +0800

    Setup model, route folder

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var app = express();
 
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Log requests to the console.
+app.use(logger('dev'));
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+require('./server/routes')(app);
 app.get('/', (req, res) => res.render('pages/index'))
 app.get('/times', (req, res) => {
 	  let result = ''
@@ -18,3 +27,10 @@ app.get('/times', (req, res) => {
 	  res.send(result)
 	});
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+
+
+
+module.exports = app;
---
+++ server/models/index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(`${__dirname}/../config/config.json`)[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize('postgres://gjjptumrojrmhp:9e21795367be032a5ce71d04f215fdab8174d8ce8ed97ad1c358a38e17d71634@ec2-75-101-153-56.compute-1.amazonaws.com:5432/d1a7f3rs7ip697',{
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }});
+} else {
+  sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+  );
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file =>
+    (file.indexOf('.') !== 0) &&
+    (file !== basename) &&
+    (file.slice(-3) === '.js'))
+  .forEach(file => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
