12a6472f99a7c58ededd448f4e0e23e48bd1b82c
weiweitoo <sengweitoo@gmail.com>
2018-09-24 23:17:08 +0800

    Modify databse configuration

--- server/models/index.js
+++ server/models/index.js
@@ -7,13 +7,13 @@ const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize('postgres://gjjptumrojrmhp:9e21795367be032a5ce71d04f215fdab8174d8ce8ed97ad1c358a38e17d71634@ec2-75-101-153-56.compute-1.amazonaws.com:5432/d1a7f3rs7ip697');
-} else {
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize('postgres://gjjptumrojrmhp:9e21795367be032a5ce71d04f215fdab8174d8ce8ed97ad1c358a38e17d71634@ec2-75-101-153-56.compute-1.amazonaws.com:5432/d1a7f3rs7ip697');
+// } else {
   sequelize = new Sequelize(
     config.database, config.username, config.password, config
   );
-}
+// }
 
 fs
   .readdirSync(__dirname)
