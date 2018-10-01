e0369ba269fc0ac2febe4414adb22e1f049bef3d
chingjunehao <junehao0722@hotmail.com>
2018-10-01 08:00:54 +0800

    Add back config.json

--- server/controllers/ClassesController.js
+++ server/controllers/ClassesController.js
@@ -31,6 +31,7 @@ module.exports = {
   getPostById(req, res){
     return Classes.findAll({attributes:[['id',"classId"]]}).then(function(classes){
             Classposts.findAll({
+                attributes:['classId', 'posttitle','postdescription','postdate'],
                 where:{
                     classId: req.params.classId
                 }
@@ -43,6 +44,7 @@ module.exports = {
   getReactionById(req, res){
     return Classposts.findAll({attributes:[['id',"classpostId"]]}).then(function(classposts){
             Reactions.findAll({
+                attributes:['classpostId', 'reactiontype'],
                 where:{
                     classId: req.params.classpostId
                 }
@@ -55,6 +57,7 @@ module.exports = {
   getCommentById(req, res){
     return Classposts.findAll({attributes:[['id',"classpostId"]]}).then(function(classposts){
             Comments.findAll({
+                attributes:['classpostId', 'content','commentdate'],
                 where:{
                     classId: req.params.classpostId
                 }
