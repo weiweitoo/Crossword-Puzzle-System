4baccccc9c9afdccd7d2fc45b0e7ef427d75b9ee
chingjunehao <junehao0722@hotmail.com>
2018-10-01 08:24:31 +0800

    Revert back changes during debug

--- server/migrations/20180924051535-create-comment.js
+++ server/migrations/20180924051535-create-comment.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       commentdate: {
-        type: Sequelize.DATE
+        type: Sequelize.STRING
       },
       userId:{
         allowNull: false,
